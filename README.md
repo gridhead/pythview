b6e67853c374a23cee1e1e3d07eed24370ea679b
Michal Konečný <mkonecny@redhat.com>
2021-05-18 13:44:22 +0200

    First version of test.py
    Signed-off-by: Michal Konečný <mkonecny@redhat.com>

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# python_interview
+# Test script for interview
 This is a test code for python interview
+
+To run this test script just run:
+
+`python test.py`
+
+and then follow the comments inside `test.py` script.
---
+++ test.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Test for interview
+
+def remove_duplicates(input: list) -> list:
+    """
+    This function removes any duplicates from list.
+
+    Test 1) Fix this function and remove any error you find.
+
+    Params:
+        input: List to remove duplicates from
+
+    Returns:
+        List without duplicates.
+    """
+    no_dup_dict = dict.fromkeys(input)
+    return no_dup_dct
+
+
+def sort_list(input: list) -> list:
+    """
+    This function sorts the input list.
+
+    Test 2) Write this function to work as expected.
+
+    Params:
+        input: List to sort
+
+    Returns:
+        Sorted list.
+    """
+    return input
+
+
+if __name__ == "__main__":
+    # Main of the python test
+    unsorted_list = [1, 2, 2, 5, 1, 3, 20, 15]
+
+    # Test 1: Remove duplicates
+    no_dup_list = remove_duplicates(unsorted_list)
+
+    # Assert that output is correct
+    assert no_dup_list == [1, 2, 5, 3, 20, 15]
+
+    # Test 2: Sort list
+    sorted_list = sort_list(no_dup_list)
+
+    # Assert that output is sorted
+    assert sorted_list == [1, 2, 3, 5, 15, 20]
